9c452d522a2518b9622564094e70b64f1c260197
rhys19 <30848481+rhys19@users.noreply.github.com>
2018-09-16 13:36:06 -0500

    Update cfg.lua
    Added Copyright

--- cfg.lua
+++ cfg.lua
@@ -1,3 +1,16 @@
+--[[
+
+-------------------------------------------------------
+Coded By Rhys19 © 2018
+Functions are not copied they are off a gta5 animation site functions are built together from docs.fivem.net & the Native Reference
+Lock Code By Knobs!
+Radar By Rhys19 (Coming Soon)
+Fix is very simple to create!
+Door controls by rhys19 -- they are simple to code aswell!
+-------------------------------------------------------
+
+--]]
+
 -- Config Isn't Working Yet!
 
 
