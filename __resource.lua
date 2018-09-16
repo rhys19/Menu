f8d14bbf563802b96d2592648f947dad039de6b1
rhys19 <30848481+rhys19@users.noreply.github.com>
2018-09-16 13:35:44 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,3 +1,16 @@
+--[[
+
+-------------------------------------------------------
+Coded By Rhys19 © 2018
+Functions are not copied they are off a gta5 animation site functions are built together from docs.fivem.net & the Native Reference
+Lock Code By Knobs!
+Radar By Rhys19 (Coming Soon)
+Fix is very simple to create!
+Door controls by rhys19 -- they are simple to code aswell!
+-------------------------------------------------------
+
+--]]
+
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
 client_scripts {
