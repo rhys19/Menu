00f15950ab00d964ec8dfe4492506cbae98cb830
rhys19 <30848481+rhys19@users.noreply.github.com>
2019-03-31 10:35:25 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,28 @@
+title = "my servername menu"
+
+MenuKey = 244 -- need key ids? get them here: https://rhys19.life/ or https://docs.fivem.net/
+
+commandEnabled = false -- trigger if you want a command menu with /menu or using a button
+
+steamWhitelist = false -- not implemented not
+pedWhitelist = false -- not implemented not
+adminWhitelist = true -- should be working ;)
+CopWhitelist = true -- should be working ;)
+AceWhitelist = false -- not implemented not
+
+admins = {
+	'steam:hexid',
+}
+
+-- add cops via steamid or discord id or license and even an IP!!
+
+cops = {
+	'steam:hexid',
+	'discord:woaDiscordIDHERE',
+	'license:R*License',
+	'ip:ipHere',
+}
+
+peds = {
+	'MP_FREEMODE_MALE'
+}
